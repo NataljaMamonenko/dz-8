6b60d6cc4ae191f86632d6a9f38daf8a16f4a4b6
NataljaMamonenko <natalja.mamonenko@gmail.com>
2023-05-04 22:26:21 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        // Создаем объекты студентов
-        Student student1 = new Student(1, "Иван", "Иванов");
-        Student student2 = new Student(2, "Петр", "Петров");
-        Student student3 = new Student(3, "Сергей", "Сергеев");
+        // Створюємо об'єкти студентів
+        Student student1 = new Student(1, "Іван", "Іванов");
+        Student student2 = new Student(2, "Петро", "Петров");
+        Student student3 = new Student(3, "Сергій", "Сергіїв");
 
-        // Создаем объект группы студентов и устанавливаем старосту
+        // Створюємо об'єкт групи студентів та обираємо старосту
         StudentGroup group = new StudentGroup(student1);
 
-        // Добавляем студентов в группу
+        // Додаємо студентів в групу
         group.addStudent(student2);
         group.addStudent(student3);
 
-        // Выводим информацию о группе
-        System.out.println("Староста группы: " + group.getGroupLeader().getFirstName());
-        System.out.println("Список студентов:");
+        // Виводимо інформацію про групу
+        System.out.println("Староста групи: " + group.getGroupLeader().getFirstName());
+        System.out.println("Список студентів:");
         for (Student student : group.getStudents()) {
             System.out.println(student.getFirstName());
         }
 
-        // Добавляем задание для группы
+        // Додаємо завдання для групи
         group.addTask("Выучить Java");
 
-       // Выводим информацию о заданиях//
+       // Виводимо информацію о завданнях//
     }
 }
