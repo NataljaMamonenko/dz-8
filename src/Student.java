b456de4762bf9818230c4b3f9fc91249e60ce006
NataljaMamonenko <natalja.mamonenko@gmail.com>
2023-05-04 20:56:15 +0300

    first commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private int id;
+    String firstName;
+    String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
